cb20f59179f76acd120b15c8fa96c8619d5d4b67
justynadabrowska <74719040+justynadabrowska@users.noreply.github.com>
2021-04-20 20:07:57 +0100

    new changes

---
+++ script.js
@@ -0,0 +1,14 @@
+import { format } from "date-fns";
+
+const datePickerButton = document.querySelector(".date-picker-button");
+const datePicker = document.querySelector(".date-picker");
+
+datePickerButton.addEventListener("click", () => {
+  datePicker.classList.toggle("show");
+});
+
+function setDate(date) {
+  datePickerButton.innerText = format(date, "MMMM do, yyyy");
+}
+
+setDate(new Date());
